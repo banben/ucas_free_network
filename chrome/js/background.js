665e5a4f216330983015cae3f1135e212bc10523
坂本 <tomlabs@163.com>
2016-07-07 21:18:53 +0800

    Update background.js

--- chrome/js/background.js
+++ chrome/js/background.js
@@ -1,6 +1,6 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        if( details.url == "http://210.77.16.21:8081/eportal/interface/index_files/pc/login_bch.js" )
+        if( details.url == "http://210.77.16.21:8080/eportal/interface/index_files/pc/login_bch.js" )
             return {redirectUrl: "chrome-extension://" + chrome.runtime.id + "/js/login_bch.js" };
     },
     {urls:["<all_urls>"]},
